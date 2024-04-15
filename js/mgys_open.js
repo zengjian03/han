433b92f371b6dc656344df826a6f0f279a82d9a2
han <zengjian0303@126.com>
2024-04-15 21:20:37 +0800

    1

---
+++ js/mgys_open.js
@@ -0,0 +1,166 @@
+import { Crypto, load, _ } from './lib/cat.js';
+
+let key = 'mgys';
+let HOST = 'https://www.moguys.xyz';
+let siteKey = '';
+let siteType = 0;
+
+const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36';
+
+async function request(reqUrl, agentSp) {
+    let res = await req(reqUrl, {
+        method: 'get',
+        headers: {
+            'User-Agent': agentSp || UA,
+            'Referer': HOST
+        },
+    });
+    return res.content;
+}
+
+// cfg = {skey: siteKey, ext: extend}
+async function init(cfg) {
+    siteKey = cfg.skey;
+    siteType = cfg.stype;
+}
+
+async function home(filter) {
+    let classes = [{"type_id":"movie","type_name":"电影"},{"type_id":"tv","type_name":"追剧"},{"type_id":"variety","type_name":"综艺"},{"type_id":"cartoon","type_name":"动漫"}];
+    let filterObj = {
+        "tv":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"古装","v":"古装"},{"n":"奇幻","v":"奇幻"},{"n":"动作","v":"动作"},{"n":"爱情","v":"爱情"},{"n":"喜剧","v":"喜剧"},{"n":"科幻","v":"科幻"},{"n":"恐怖","v":"恐怖"},{"n":"战争","v":"战争"},{"n":"武侠","v":"武侠"},{"n":"惊悚","v":"惊悚"},{"n":"悬疑","v":"悬疑"},{"n":"冒险","v":"冒险"},{"n":"犯罪","v":"犯罪"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"韩国","v":"韩国"},{"n":"美国","v":"美国"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"国语","v":"国语"},{"n":"英语","v":"英语"},{"n":"粤语","v":"粤语"},{"n":"闽南语","v":"闽南语"},{"n":"韩语","v":"韩语"},{"n":"日语","v":"日语"},{"n":"其它","v":"其它"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
+        "movie":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"动作","v":"动作"},{"n":"爱情","v":"爱情"},{"n":"喜剧","v":"喜剧"},{"n":"科幻","v":"科幻"},{"n":"恐怖","v":"恐怖"},{"n":"战争","v":"战争"},{"n":"武侠","v":"武侠"},{"n":"剧情","v":"剧情"},{"n":"动画","v":"动画"},{"n":"惊悚","v":"惊悚"},{"n":"灾难","v":"灾难"},{"n":"悬疑","v":"悬疑"},{"n":"冒险","v":"冒险"},{"n":"犯罪","v":"犯罪"},{"n":"纪录","v":"纪录"},{"n":"古装","v":"古装"},{"n":"奇幻","v":"奇幻"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"韩国","v":"韩国"},{"n":"美国","v":"美国"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"日本","v":"日本"},{"n":"泰国","v":"泰国"},{"n":"英国","v":"英国"},{"n":"新加坡","v":"新加坡"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010","v":"2010"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"国语","v":"国语"},{"n":"英语","v":"英语"},{"n":"粤语","v":"粤语"},{"n":"闽南语","v":"闽南语"},{"n":"韩语","v":"韩语"},{"n":"日语","v":"日语"},{"n":"法语","v":"法语"},{"n":"德语","v":"德语"},{"n":"其它","v":"其它"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
+        "cartoon":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"选秀","v":"选秀"},{"n":"情感","v":"情感"},{"n":"访谈","v":"访谈"},{"n":"播报","v":"播报"},{"n":"旅游","v":"旅游"},{"n":"音乐","v":"音乐"},{"n":"美食","v":"美食"},{"n":"纪实","v":"纪实"},{"n":"曲艺","v":"曲艺"},{"n":"生活","v":"生活"},{"n":"游戏互动","v":"游戏互动"},{"n":"财经","v":"财经"},{"n":"求职","v":"求职"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"韩国","v":"韩国"},{"n":"美国","v":"美国"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"日本","v":"日本"},{"n":"泰国","v":"泰国"},{"n":"英国","v":"英国"},{"n":"新加坡","v":"新加坡"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"国语","v":"国语"},{"n":"英语","v":"英语"},{"n":"粤语","v":"粤语"},{"n":"闽南语","v":"闽南语"},{"n":"韩语","v":"韩语"},{"n":"日语","v":"日语"},{"n":"其它","v":"其它"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],
+        "variety":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"情感","v":"情感"},{"n":"科幻","v":"科幻"},{"n":"热血","v":"热血"},{"n":"推理","v":"推理"},{"n":"搞笑","v":"搞笑"},{"n":"冒险","v":"冒险"},{"n":"萝莉","v":"萝莉"},{"n":"校园","v":"校园"},{"n":"动作","v":"动作"},{"n":"机战","v":"机战"},{"n":"运动","v":"运动"},{"n":"战争","v":"战争"},{"n":"少年","v":"少年"},{"n":"少女","v":"少女"},{"n":"社会","v":"社会"},{"n":"原创","v":"原创"},{"n":"亲子","v":"亲子"},{"n":"益智","v":"益智"},{"n":"励志","v":"励志"},{"n":"其他","v":"其他"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"韩国","v":"韩国"},{"n":"美国","v":"美国"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"日本","v":"日本"},{"n":"泰国","v":"泰国"},{"n":"英国","v":"英国"},{"n":"新加坡","v":"新加坡"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"国语","v":"国语"},{"n":"英语","v":"英语"},{"n":"粤语","v":"粤语"},{"n":"闽南语","v":"闽南语"},{"n":"韩语","v":"韩语"},{"n":"日语","v":"日语"},{"n":"其它","v":"其它"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]
+	};
+
+    return JSON.stringify({
+        class: classes,
+        filters: filterObj,
+    });
+}
+
+async function homeVod() {}
+
+async function category(tid, pg, filter, extend) {
+    if (pg <= 0) pg = 1;
+    const link = HOST + '/vodshow/' + tid + '-' + (extend.area || '') + '-' + (extend.by || 'time') + '-' + (extend.class || '') + '-' + (extend.lang || '') + '----' + pg + '---' + (extend.year || '') + '.html';//https://www.moguys.xyz/vodshow/tv-%E5%A4%A7%E9%99%86-hits-%E5%8A%A8%E4%BD%9C-%E5%9B%BD%E8%AF%AD-------2022.html
+    const html = await request(link);
+    const $ = load(html);
+    const items = $('ul.stui-vodlist > li');
+    let videos = _.map(items, (item) => {
+        const it = $(item).find('a:first')[0];
+        const remarks = $($(item).find('span.pic-text text-right')[0]).text().trim();
+        return {
+            vod_id: it.attribs.href.replace(/.*?\/voddetail\/(.*).html/g, '$1'),
+            vod_name: it.attribs.title,
+            vod_pic: it.attribs['data-original'],
+            vod_remarks: remarks || '',
+        };
+    });
+    const hasMore = $('ul.stui-page__item > li > a:contains(下一页)').length > 0;
+    const pgCount = hasMore ? parseInt(pg) + 1 : parseInt(pg);
+    return JSON.stringify({
+        page: parseInt(pg),
+        pagecount: pgCount,
+        limit: 24,
+        total: 24 * pgCount,
+        list: videos,
+    });
+}
+
+async function detail(id) {
+    const html = await request( HOST + '/voddetail/' + id + '.html');
+    const $ = load(html);
+    const vod = {
+        vod_id: id,
+        vod_name: $('h1:first').text().trim(),
+        vod_type: $('.stui-content__detail p:first a').text(),
+        vod_actor: $('.stui-content__detail p:nth-child(3)').text().replace('主演：',''),
+        vod_pic: $('.stui-content__thumb img:first').attr('data-original'),
+        vod_remarks : $('.stui-content__detail p:nth-child(5)').text() || '',
+        vod_content: $('span.detail-content').text().trim(),
+    };
+    let playMap = {};
+    const tabs = $('div.stui-pannel__head > h3');
+    const playlists = $('ul.stui-content__playlist');
+    _.each(tabs, (tab, i) => {
+        const from = tab.children[0].data.replace(/\s+/g,"");
+        let list = playlists[i];
+        list = $(list).find('a');
+        _.each(list, (it) => {
+            const title = it.children[0].data;
+            const playUrl = it.attribs.href.replace(/\/vodplay\/(.*).html/g, '$1');
+            if (title.length == 0) title = it.children[0].data.trim();
+            if (!playMap.hasOwnProperty(from)) {
+                playMap[from] = [];
+            }
+            playMap[from].push( title + '$' + playUrl);
+        });
+    });
+    vod.vod_play_from = _.keys(playMap).join('$$$');
+    const urls = _.values(playMap);
+    let vod_play_url = _.map(urls, (urlist) => {
+        return urlist.join('#');
+    });
+    vod.vod_play_url = vod_play_url.join('$$$');
+    return JSON.stringify({
+        list: [vod],
+    });
+}
+async function play(flag, id, flags) {
+    const link = HOST + '/vodplay/' + id + '.html';
+    const html = await request(link);
+    const $ = load(html);
+    const js = JSON.parse($('script:contains(player_)').html().replace('var player_aaaa=',''));
+    const playUrl = unescape(base64Decode(js.url))
+   
+    return JSON.stringify({
+        parse: 0,
+        url: playUrl,
+    });
+}
+
+function base64Encode(text) {
+    return Crypto.enc.Base64.stringify(Crypto.enc.Utf8.parse(text));
+}
+
+function base64Decode(text) {
+    return Crypto.enc.Utf8.stringify(Crypto.enc.Base64.parse(text));
+}
+async function search(wd, quick, pg) {
+    if (pg <= 0) pg = 1;
+    let data = await request(HOST + '/vodsearch/' + wd + '----------' + pg + '---' + '.html');//https://www.moguys.xyz/vodsearch/%E6%88%91----------2---.html
+    const $ = load(data);
+      const items = $('ul.stui-vodlist > li');
+    let videos = _.map(items, (item) => {
+        const it = $(item).find('a:first')[0];
+        const remarks = $($(item).find('span.pic-text text-right')[0]).text().trim();
+        return {
+            vod_id: it.attribs.href.replace(/.*?\/voddetail\/(.*).html/g, '$1'),
+            vod_name: it.attribs.title,
+            vod_pic: it.attribs['data-original'],
+            vod_remarks: remarks || '',
+        };
+    });
+    const hasMore = $('ul.stui-page__item > li > a:contains(下一页)').length > 0;
+    const pgCount = hasMore ? parseInt(pg) + 1 : parseInt(pg);
+    return JSON.stringify({
+        page: parseInt(pg),
+        pagecount: pgCount,
+        limit: 24,
+        total: 24 * pgCount,
+        list: videos,
+    });
+}
+
+export function __jsEvalReturn() {
+    return {
+        init: init,
+        home: home,
+        homeVod: homeVod,
+        category: category,
+        detail: detail,
+        play: play,
+        search: search,
+    };
+}
