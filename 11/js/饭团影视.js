75a0d29dc72da76d7797b041e08590ef334f8791
jian <zengjian0303@126.com>
2026-03-19 15:44:34 +0800

    Signed-off-by: jian <zengjian0303@126.com>

--- 11/js/饭团影视.js
+++ 11/js/饭团影视.js
@@ -1,17 +1,48 @@
 var rule = {
-    模板: 'vfed',
-    title: '饭团影视',
-    host: 'https://fantuansjz.com/',
-    url: '/sjvodshow/fyclass--------fypage---.html',
-    searchUrl: '/sjvodsearch/**----------fypage---.html',
-    class_parse: '.fed-navs-left&&a;a&&Text;a&&href;/sjvodtype/(.*?).html',
-    二级: {
-        title: 'h3&&Text;.fed-deta-content&&li:eq(2)--span&&Text',
-        img: '.fed-list-pics&&data-original',
-        desc: '.fed-list-remarks&&Text;.fed-deta-content&&li:eq(4)--span&&Text;.fed-deta-content&&li:eq(3)--span&&Text;.fed-deta-content&&li--span&&Text;.fed-deta-content&&li:eq(1)--span&&Text',
-        content: '',
-        tabs: 'body&&.fed-tabs-btn',
-        lists: 'body&&.fed-tabs-btm:eq(#id)&&li',
+    title:'饭团影视',
+    模板:'vfed',
+    host:'https://fantuansjz.com',
+    homeUrl:'/',
+    url: '/sjvodtype/fyclass/page/fypage.html',
+    searchUrl: '/sjvodsearch/**------------fypage---.html',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+        'User-Agent': 'UC_UA'
     },
-    搜索: 'body&&.fed-part-layout&&.fed-list-deta;h3&&Text;*;*;.fed-deta-play&&href;.fed-part-rows&&Text',
+    class_parse: '.fed-pops-navbar&&ul.fed-pops-list&&li;a&&Text;a&&href;.*/(.*?).html',
+    play_parse: true,
+    lazy: `js:
+  let html = request(input);
+  let hconf = html.match(/r player_.*?=(.*?)</)[1];
+  let json = JSON5.parse(hconf);
+  let url = json.url;
+  if (json.encrypt == '1') {
+    url = unescape(url);
+  } else if (json.encrypt == '2') {
+    url = unescape(base64Decode(url));
+  }
+  if (/\.(m3u8|mp4|m4a|mp3)/.test(url)) {
+    input = {
+      parse: 0,
+      jx: 0,
+      url: url,
+    };
+  } else {
+    input;
+  }`,
+    limit: 6,
+    double: true,
+    推荐: 'ul.fed-list-info.fed-part-rows;li;a.fed-list-title&&Text;a.fed-list-pics&&data-original;.fed-list-remarks&&Text;a&&href',
+    一级: '.fed-list-info&&li;a.fed-list-title&&Text;a.fed-list-pics&&data-original;.fed-list-remarks&&Text;a&&href',
+    二级: {
+        title: 'h3.fed-part-eone&&Text;.fed-deta-content&&li:eq(2)&&a&&Text',
+        img: '.fed-deta-images&&a.fed-list-pics&&data-original',
+        desc: '.fed-deta-content&&li:eq(1)&&Text;.fed-deta-content&&li:eq(2)&&a&&Text;.fed-deta-content&&li:eq(3)&&a&&Text;.fed-deta-content&&li:eq(4)&&a&&Text;.fed-deta-content&&li:eq(5)&&a&&Text',
+        content: '.fed-conv-text&&Text',
+        tabs: '.fed-tabs-foot&&ul.fed-part-rows&&li',
+        lists: '.fed-tabs-btm:eq(#id)&&li',
+    },
+    搜索: '.fed-list-info;h3.fed-part-eone&&Text;a.fed-list-pics&&data-original;.fed-list-remarks&&Text;a&&href;.fed-deta-content&&Text',
 }
